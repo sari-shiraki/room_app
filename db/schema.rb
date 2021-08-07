25315ae3dfd2763809f5ee8b3d84eb9fe249d748
EC2 Default User <ec2-user@ip-172-31-21-237.us-east-2.compute.internal>
2021-08-07 07:38:27 +0000

    userにnameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210807055136) do
+ActiveRecord::Schema.define(version: 20210807073039) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "finish_date"
+    t.integer  "number_of_people"
+    t.integer  "room_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "room_name"
+    t.text     "room_introduction"
+    t.integer  "price"
+    t.string   "adress"
+    t.integer  "user_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -20,6 +39,7 @@ ActiveRecord::Schema.define(version: 20210807055136) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
