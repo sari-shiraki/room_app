92869a785a3c7efa784a356f3107e73ba77e80e3
EC2 Default User <ec2-user@ip-172-31-21-237.us-east-2.compute.internal>
2021-08-07 06:54:20 +0000

    reservationモデル、コントローラー実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+
+  get 'reservations/new'
+
+  get 'reservations/show'
+
+  get 'index/add'
+
+  get 'index/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
