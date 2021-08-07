838ca5e02582b9862fd18f957060bfd662871733
EC2 Default User <ec2-user@ip-172-31-21-237.us-east-2.compute.internal>
2021-08-07 06:59:58 +0000

    only_Reservationの予約機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   get 'reservations/index'
+  get '/reservations', to:'reservations#index'
 
   get 'reservations/new'
+  post 'reservations/new', to: 'reservations#create'
 
-  get 'reservations/show'
-
-  get 'index/add'
-
-  get 'index/show'
+  get 'reservations/:id', to: 'reservations#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
