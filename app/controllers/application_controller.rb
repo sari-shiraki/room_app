f034578005bb1a197d9de4a0c5c6cb280a1151ab
EC2 Default User <ec2-user@ip-172-31-21-237.us-east-2.compute.internal>
2021-08-09 08:54:55 +0000

    キーワード検索実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller? 
   
+  #deviseの遷移先変更
+  
   def after_sign_in_path_for(resource)
     home_path(resource)
   end
@@ -16,7 +18,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
