1d13589d2a5c52cf931516fc3be99faabbda61b1
EC2 Default User <ec2-user@ip-172-31-21-237.us-east-2.compute.internal>
2021-08-11 08:24:40 +0000

    ユーザーにプロフ画像追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path(resource)
   end
+  
+  def after_update_path_for(resource)
+    edit_user_registration_path(resource)
+  end
 
 
   
@@ -28,6 +32,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,5 @@ class UsersController < ApplicationController
     @rooms = @user.rooms
   end 
   
-  private
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end  
-  
   
 end
